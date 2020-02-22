890df2d6343c95e4f87ebefaa1bf537d5cca2758
An Rodriguez <anmichel@gmail.com>
2020-02-22 18:54:19 -0400

    moving files to same folder structure as it's being processed

--- ordenar-tobo.py
+++ ordenar-tobo.py
@@ -8,8 +8,11 @@ from tqdm import tqdm
 import shutil
 import sys
 
-files = glob.glob(os.path.join('tobo','*.jpg'))
-# print(files[0])
+files = glob.glob(os.path.join('..', 'tobo','*.jpg'))
+# srcdir = os.path.join('..', 'tobo-ordenado','*', '*', '*.jpg')
+# print(srcdir)
+# files = glob.glob(srcdir)
+# print(len(files))
 # sys.exit()
 c=0
 for file in tqdm(files):
@@ -17,8 +20,8 @@ for file in tqdm(files):
 		c+=1
 		src = file
 		basename = os.path.basename(src)
-		# basedir = os.path.dirname(src)
-		basedir = 'tobo-ordenado'
+		basedir = os.path.dirname(src)
+
 		fsize = os.path.getsize(src)		
 		if fsize == 0:
 			zerodir = os.path.join(basedir, 'size-zero')
@@ -40,25 +43,37 @@ for file in tqdm(files):
 
 		# datetime = image.datetime
 		datestr = datetime.split(" ")[0]
+		timestr = datetime.split(" ")[1]
 		year,month,day = datestr.split(":")
-		
+		hour,minutes,seconds = timestr.split(":")
+		basedir = 'tobo-ordenado'
 		dirname = f"{year}-{month}-{day}"
-		dstdir = os.path.join(basedir, dirname)
-		if not os.path.exists(dstdir):
-			os.mkdir(dstdir)
-			
 		
-		dst = os.path.join(basedir, dirname, basename)
-		# print(f"{src}->{dst}")
-		shutil.move(src, dst)
+		minutes = '00' if int(minutes) < 30 else '30'
+
+		
+		subdirname = f"n{year[-2:]}{month}{day}_{hour}{minutes}"
+		dstdir = os.path.join('..', basedir, dirname, subdirname)
+		# tqdm.write(dstdir)
+		
+		if not os.path.exists(dstdir):
+			os.makedirs(dstdir)
+		
+		try:
+			# tqdm.write(f"{src}->{dstdir}")
+			shutil.move(src, dstdir)
+			# sys.exit()
+		except Exception as error:
+			if "already exists" in str(error):
+				print(f"Ignoring: {error}")
 		
 		# if c>100:
 			# break
 
 		# break
 	except Exception as err:
-		print(file)
-		pprint(dir(image))
-		print(err)
+		tqdm.write(file)
+		# tqdm.write(dir(image))
+		tqdm.write(err)
 		raise(err)
 		
