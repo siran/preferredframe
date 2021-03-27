0cfa717f8d23b775778b1fa93d24a2007bbdc8eb
An Rodriguez <anmichel@gmail.com>
2021-03-27 14:37:46 -0400

    local changes to ordenar tobo

--- ordenar-tobo.py
+++ ordenar-tobo.py
@@ -9,9 +9,9 @@ import shutil
 import sys
 
 files = glob.glob(os.path.join('..', 'tobo','*.jpg'))
-# srcdir = os.path.join('..', 'tobo-ordenado','*', '*', '*.jpg')
+srcdir = os.path.join('..', 'tobo-ordenado','*', '*.jpg')
 # print(srcdir)
-# files = glob.glob(srcdir)
+files = glob.glob(srcdir)
 # print(len(files))
 # sys.exit()
 c=0
@@ -22,21 +22,21 @@ for file in tqdm(files):
 		basename = os.path.basename(src)
 		basedir = os.path.dirname(src)
 
-		fsize = os.path.getsize(src)		
+		fsize = os.path.getsize(src)
 		if fsize == 0:
 			zerodir = os.path.join(basedir, 'size-zero')
 			if not os.path.exists(zerodir):
 				os.mkdir(zerodir)
-				
+
 			dst = os.path.join(zerodir, basename)
 			# print(f"{src}->{dst}")
-			shutil.move(src, dst)		
-			continue		
-		
+			shutil.move(src, dst)
+			continue
+
 		image = Image(open(file,'rb'))
 		# pprint(dir(image))
 		datetime = image.get("datetime")
-		if not datetime:	
+		if not datetime:
 			print('no date time')
 			print(src)
 			break
@@ -48,17 +48,17 @@ for file in tqdm(files):
 		hour,minutes,seconds = timestr.split(":")
 		basedir = 'tobo-ordenado'
 		dirname = f"{year}-{month}-{day}"
-		
+
 		minutes = '00' if int(minutes) < 30 else '30'
 
-		
+
 		subdirname = f"n{year[-2:]}{month}{day}_{hour}{minutes}"
 		dstdir = os.path.join('..', basedir, dirname, subdirname)
 		# tqdm.write(dstdir)
-		
+
 		if not os.path.exists(dstdir):
 			os.makedirs(dstdir)
-		
+
 		try:
 			# tqdm.write(f"{src}->{dstdir}")
 			shutil.move(src, dstdir)
@@ -66,7 +66,7 @@ for file in tqdm(files):
 		except Exception as error:
 			if "already exists" in str(error):
 				print(f"Ignoring: {error}")
-		
+
 		# if c>100:
 			# break
 
@@ -76,4 +76,4 @@ for file in tqdm(files):
 		# tqdm.write(dir(image))
 		tqdm.write(err)
 		raise(err)
-		
+
