d61162478bc414307778815914a6643050fd8dc7
An Rodriguez <anmichel@gmail.com>
2024-05-22 14:48:09 -0400

    plotting height of moon and sun. todo: add distance plot

--- matlab-libraries/u18/plotheightstar.m
+++ matlab-libraries/u18/plotheightstar.m
@@ -1,5 +1,5 @@
 % plot height of star
-if false && exist('timeAltitude') && ~isempty(timeAltitude)
+if exist('timeAltitude') && ~isempty(timeAltitude)
     holdStatus = ishold;
     hold on
     ax = gca;
@@ -8,29 +8,43 @@ if false && exist('timeAltitude') && ~isempty(timeAltitude)
     ax.YColor = [0 0 0];
     ylimites = [-90 90];
     ylim(ylimites)
-    
+
     ax = gca;
     ax.YTick = [-90 -80:20:80 90];
-    
+
     vertical_steps = -90:180/20:90;
     ylabel('Altitude of stellar objects')
 
     if ax.XLim == [0 360]
-        altitudes = pchip(timeAltitude(:,1), timeAltitude(:,2), ...
-            xts(1):(xts(end)-xts(1))/360:xts(end));
-
-        azimuths = pchip(timeAltitude(:,1), timeAltitude(:,3), ...
-            xts(1):(xts(end)-xts(1))/20:xts(end));
-        
-        plot(0:360, altitudes, 'b-', 'lineWidth', 3)
-        plot(azimuths, vertical_steps,'b-', 'lineWidth', 3)                  
-    else
-        colors = {'b','y','k'};
+        colors = {'b', 'y', 'k'};
         stars = {'HIP54589', 'Sun', 'Moon'};
-        for starname_i = 1:size(timeAltitude,3)
-            plot(timeAltitude(:,1, starname_i),timeAltitude(:,2, starname_i),[colors{starname_i} '.-'],'lineWidth',3)
+        plot_handles = gobjects(size(timeAltitude,3), 1); % Preallocate handles
+
+        for starname_i = 1:numel(stars)
+            altitudes = pchip(timeAltitude(:,1, starname_i), timeAltitude(:,2, starname_i), ...
+                xts(1):(xts(end)-xts(1))/360:xts(end));
+
+            azimuths = pchip(timeAltitude(:,1, starname_i), timeAltitude(:,3, starname_i), ...
+                xts(1):(xts(end)-xts(1))/20:xts(end));
+
+            plot_handles(starname_i) = plot(0:360, altitudes, [colors{starname_i} '.-'], 'lineWidth', 3);
+            hold on; % Hold the plot to add multiple lines to the same plot
+            plot(azimuths, vertical_steps, [colors{starname_i} '.-'], 'lineWidth', 3);
         end
-        legend(ax, [stars])
+
+        legend(plot_handles, stars);% Use plot handles in legend
+
+    else
+        colors = {'b', 'y', 'k'};
+        stars = {'HIP54589', 'Sun', 'Moon'};
+        plot_handles = gobjects(size(timeAltitude,3), 1); % Preallocate handles
+
+        for starname_i = 1:size(timeAltitude, 3)
+            plot_handles(starname_i) = plot(timeAltitude(:, 1, starname_i), timeAltitude(:, 2, starname_i), ...
+                [colors{starname_i} '.-'], 'lineWidth', 3);
+        end
+
+        legend(plot_handles, stars); % Use plot handles in legend
     end
     % legend([{'Fringe shift'}, stars])
     yyaxis left
