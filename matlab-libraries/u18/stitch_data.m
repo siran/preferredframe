2949b908b0363c53ad377f6702d4d7525db773c6
An Rodriguez <anmichel@gmail.com>
2024-07-01 17:10:44 -0400

    script to gather all data in struct - just sent .mat to Nathan via email with feb 2020 data

---
+++ matlab-libraries/u18/stitch_data.m
@@ -0,0 +1,41 @@
+% stitches pre-analized data
+% uses the saved workspaces
+
+% default variables
+
+
+clc
+
+pictures_base_folder = 'D:\Users\an\experimento-usb-interferometro\tobo-ordenado'
+workspace_folder = 'D:\Users\an\experimento-usb-interferometro\u18_workspaces';
+
+workspaces = dir([workspace_folder '\wn2002??_????-adjusted_orig.mat']);
+
+p_struct = struct();
+% figure 
+% hold on
+for wsi = 1:length(workspaces)
+    ws = workspaces(wsi);
+    % disp("Stitching " + ws.name);
+    
+    clear p_adjusted_orig lineaPreferida fconf conf;
+
+    load([ws.folder '\' replace(ws.name, '-adjusted_orig', '')]);
+    load([ws.folder '\' ws.name]);
+
+    if exist(fconf, "file")
+        conf = readtable(fconf);   
+        p_struct(wsi).p = p_adjusted_orig(:, conf.lineaPreferida);
+        p_struct(wsi).linea_preferida = p_adjusted_orig(:, conf.lineaPreferida);
+        p_struct(wsi).linea_preferidaAdjunta = p_adjusted_orig(:, conf.lineaPreferidaAdjunta);
+    end
+
+    p_struct(wsi).p_all = p_adjusted_orig;
+    p_struct(wsi).xts = xts;
+    p_struct(wsi).size = length(xts);
+
+    % plot(p_struct(wsi).xts, p_struct(wsi).p_all);
+
+    % refresh
+    % a=1:
+end
